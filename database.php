e64277ab9bf7d7573e218e3b780fb72f074ccd8a
talhazaryabDA6~~ <zaryabdev404@gmail.com>
2024-06-08 00:50:47 +0500

    Added notes

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $hostName = "localhost";
 $dbUser = "root";
 $dbPassword = "";
-$dbName = "login_register";
+$dbName = "travelitineraryplanner";
 $conn = mysqli_connect($hostName, $dbUser, $dbPassword, $dbName);
 if (!$conn) {
     die("Something went wrong;");
---
+++ notes.md
@@ -0,0 +1,19 @@
+users
+
+# flights
+
+    title
+    rate
+    rating
+
+# hotels
+
+    title
+    rate per day
+    rating
+
+# attractions
+
+    title
+    rate per day
+    rating
